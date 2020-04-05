371b665db8543f01606f958481f0ef74e4d096d9
Artem <maz@2i.tusur.ru>
2020-04-05 23:51:17 +0700

    updersion for functions submit_for_verification

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version	= 2020032101;		// The current module version (Date: YYYYMMDDXX)
+$plugin->version	= 2020040500;		// The current module version (Date: YYYYMMDDXX)
 $plugin->requires	= 2018050800;		// Requires this Moodle version
 $plugin->component	= 'mod_codeview';	// Full name of the plugin (used for diagnostics)
